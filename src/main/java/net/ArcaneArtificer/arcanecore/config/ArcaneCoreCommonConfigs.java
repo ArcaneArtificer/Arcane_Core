55da081635136bff580e8b05a06e6a10d62def7c
ArcaneArtificer <thearcaneartificer@gmail.com>
2022-08-21 11:46:29 -0700

    Updated ArcanicEnchant class for future support in ImprovedVillagers and AllTheBooks packs.

--- src/main/java/net/ArcaneArtificer/arcanecore/config/ArcaneCoreCommonConfigs.java
+++ src/main/java/net/ArcaneArtificer/arcanecore/config/ArcaneCoreCommonConfigs.java
@@ -5,8 +5,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class ArcaneCoreCommonConfigs {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
-
-    public static final ForgeConfigSpec.ConfigValue<Boolean> CLEAR_VANILLA_TRADES;
     public static final ForgeConfigSpec.ConfigValue<Integer> NETHER_BERYL_SMALL_VEIN_SIZE;
     public static final ForgeConfigSpec.ConfigValue<Integer> NETHER_BERYL_SMALL_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> NETHER_BERYL_MEDIUM_VEIN_SIZE;
@@ -17,14 +15,15 @@ public class ArcaneCoreCommonConfigs {
     public static final ForgeConfigSpec.ConfigValue<Integer> NETHER_BERYL_END_VEINS_PER_CHUNK;
     public static final ForgeConfigSpec.ConfigValue<Integer> END_TANZANITE_VEIN_SIZE;
     public static final ForgeConfigSpec.ConfigValue<Integer> END_TANZANITE_VEINS_PER_CHUNK;
-    public static final ForgeConfigSpec.ConfigValue<Double> PIGLIN_WORKSTATION_BARTERING_RATE;
     public static final ForgeConfigSpec.ConfigValue<Integer> DIFFICULTY;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> CLEAR_VANILLA_TRADES;
     public static final ForgeConfigSpec.ConfigValue<Boolean> MAX_LEVEL_ENCHANTMENTS;
+    public static final ForgeConfigSpec.ConfigValue<Double> PIGLIN_WORKSTATION_BARTERING_RATE;
 
     static {
         BUILDER.push("Configs");
 
-        CLEAR_VANILLA_TRADES = BUILDER.comment("Replaces vanilla trades with more balanced trades (Vanilla traders only).").define("Vanilla Trades", Boolean.FALSE);
+        BUILDER.comment("Generic Configs:");
         NETHER_BERYL_SMALL_VEIN_SIZE = BUILDER.comment("Nether Beryl Small Vein Size").defineInRange("Small Nether Beryl Vein Size", 4, 2, 10);
         NETHER_BERYL_SMALL_VEINS_PER_CHUNK = BUILDER.comment("Nether Beryl Small Veins Per Chunk").defineInRange("Small Nether Beryl Veins Per Chunk", 6, 2, 12);
         NETHER_BERYL_MEDIUM_VEIN_SIZE = BUILDER.comment("Nether Beryl Medium Vein Size").defineInRange("Medium Nether Beryl Vein Size", 6, 2, 15);
@@ -35,9 +34,12 @@ public class ArcaneCoreCommonConfigs {
         NETHER_BERYL_END_VEINS_PER_CHUNK = BUILDER.comment("Nether Beryl End Veins Per Chunk").defineInRange("End Nether Beryl Veins Per Chunk", 6, 2, 12);
         END_TANZANITE_VEIN_SIZE = BUILDER.comment("End Tanzanite Vein Size").defineInRange("End Nether Beryl Vein Size", 8, 2, 20);
         END_TANZANITE_VEINS_PER_CHUNK = BUILDER.comment("End Tanzanite Veins Per Chunk").defineInRange("End Nether Beryl Veins Per Chunk", 8, 2, 12);
-        PIGLIN_WORKSTATION_BARTERING_RATE = BUILDER.comment("Percent chance to get villager trading post in addition to current bartering item").defineInRange("Piglin Bartering For Trading Post Percent", 0.01, 0.0, 0.1);
         DIFFICULTY = BUILDER.comment("Difficulty setting.  1 - Casual, 5 - Typical Use, 10 - SMP Balancing").defineInRange("Arcane Mod Difficulty", 5, 1, 10);
+
+        BUILDER.comment("Improved Villager Specific Configs:");
+        CLEAR_VANILLA_TRADES = BUILDER.comment("Replaces vanilla trades with more balanced trades.").define("Vanilla Trades", Boolean.FALSE);
         MAX_LEVEL_ENCHANTMENTS = BUILDER.comment("Guarantees max level enchants from trading (books and gear).").define("Max Level Enchants", Boolean.FALSE);
+        PIGLIN_WORKSTATION_BARTERING_RATE = BUILDER.comment("Percent chance to get villager trading post in addition to current bartering item").defineInRange("Piglin Bartering For Trading Post Percent", 0.01, 0.0, 0.1);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
--- src/main/java/net/ArcaneArtificer/arcanecore/enchantment/ArcanicEnchant.java
+++ src/main/java/net/ArcaneArtificer/arcanecore/enchantment/ArcanicEnchant.java
@@ -1,36 +1,58 @@
 package net.ArcaneArtificer.arcanecore.enchantment;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.*;
 
 public class ArcanicEnchant extends Enchantment {
     private final CustomRarity customRarity;
     private final Boolean treasure;
     private final Boolean curse;
 
-    public ArcanicEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots) {
-        this(pRarity, pCategory, pApplicableSlots, CustomRarity.COMMON, false);
+    private final Map<Integer, ArrayList<VillagerProfession>> TRADER_LEVEL_TO_PROFESSION = new HashMap<>();
+
+    public ArcanicEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
+        this(CustomRarity.COMMON, false, false, pRarity, pCategory, pApplicableSlots);
     }
 
-    public ArcanicEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots, CustomRarity pCustomRarity) {
-        this(pRarity, pCategory, pApplicableSlots, pCustomRarity, false);
+    public ArcanicEnchant(CustomRarity pCustomRarity, Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
+        this(pCustomRarity, false, false, pRarity, pCategory, pApplicableSlots);
     }
 
-    public ArcanicEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots, CustomRarity pCustomRarity,
-                          boolean pTreasure) {
-        this(pRarity, pCategory, pApplicableSlots, pCustomRarity, false, false);
+    public ArcanicEnchant(CustomRarity pCustomRarity, boolean isTreasure, Rarity pRarity, EnchantmentCategory pCategory,
+                          EquipmentSlot... pApplicableSlots) {
+        this(pCustomRarity, isTreasure, false, pRarity, pCategory, pApplicableSlots);
     }
 
-    public ArcanicEnchant(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots, CustomRarity pCustomRarity,
-                          boolean pTreasure, boolean isCurse) {
+    public ArcanicEnchant(CustomRarity pCustomRarity, boolean isTreasure, boolean isCurse, Rarity pRarity, EnchantmentCategory pCategory,
+                          EquipmentSlot... pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
         customRarity = pCustomRarity;
-        treasure = pTreasure;
+        treasure = isTreasure;
         curse = isCurse;
+        TRADER_LEVEL_TO_PROFESSION.put(1, new ArrayList<>(List.of(VillagerProfession.LIBRARIAN)));
+        TRADER_LEVEL_TO_PROFESSION.put(2, new ArrayList<>(List.of(VillagerProfession.LIBRARIAN)));
+        TRADER_LEVEL_TO_PROFESSION.put(3, new ArrayList<>(List.of(VillagerProfession.LIBRARIAN)));
+        TRADER_LEVEL_TO_PROFESSION.put(4, new ArrayList<>(List.of(VillagerProfession.LIBRARIAN)));
+        TRADER_LEVEL_TO_PROFESSION.put(5, new ArrayList<>(List.of(VillagerProfession.LIBRARIAN)));
+    }
+
+    public ArcanicEnchant(Map<Integer, ArrayList<VillagerProfession>> pTrade_Level_to_Profession, CustomRarity pCustomRarity, boolean isTreasure,
+                          boolean isCurse, Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
+        super(pRarity, pCategory, pApplicableSlots);
+        customRarity = pCustomRarity;
+        treasure = isTreasure;
+        curse = isCurse;
+        TRADER_LEVEL_TO_PROFESSION.putAll(pTrade_Level_to_Profession);
     }
 
     public ArcanicEnchant.CustomRarity getCustomRarity() {return this.customRarity;}
+
+    public Map<Integer, ArrayList<VillagerProfession>> get_profession_trades() {return this.TRADER_LEVEL_TO_PROFESSION;}
 
     @Override
     public boolean isTreasureOnly() {return this.treasure;}
